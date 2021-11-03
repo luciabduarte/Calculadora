9f5d32041603c8c996280410155c04caa03f58e7
Duarte <lucia.duarte@vetta.digital>
2021-11-03 17:35:21 -0300

    Adicionamento da matemática - bascara

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int x=8, y=9;
-        Calculadora operate = new Calculadora(x, y);
+        int x = 8, y = 9, z = 10;
+        Calculadora operate = new Calculadora(x, y, z);
         System.out.printf("Soma: %d\n", operate.soma());
         System.out.printf("Subtracao: %d\n", operate.subtracao());
         System.out.printf("Divisao: %f\n", operate.divisao());
         System.out.printf("Multiplicacao: %f\n", operate.multiplicacao());
-        //operate.imprime();
+        operate.bascara();
+        // operate.imprime();
+        // rodar o sistema
+        // javac Main.java
+        // java Main
     }
 }
